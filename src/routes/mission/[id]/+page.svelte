2d6342ff288f60e5ca43a488414778800ea2a30c
Carl Omondi <carlkomondi@gmail.com>
2025-01-14 23:57:33 -0600

    Added signature

--- src/routes/mission/[id]/+page.svelte
+++ src/routes/mission/[id]/+page.svelte
@@ -2,6 +2,13 @@
 
 
 
-<main class="h-screen w-screen border-orange-500"></main>
+<main class="h-screen w-screen border border-orange-500">
+    
+</main>
+
+<footer class="fixed bottom-0 left-0 right-0 px-5 py-3 flex justify-between text-accent-foreground opacity-50 text-xs">
+    <h6>Longhorn Rocketry Association &middot; Argus &trade;</h6>
+    <h6>University of Texas at Austin <span>&#10085;</span> { new Date().getFullYear() }</h6>
+</footer>
 
 
